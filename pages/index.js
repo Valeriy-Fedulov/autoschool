e1a4f0e5a59bb8cfe7b83dd76f108a12ef61c50a
vofm <valerafm.vofm@gmail.com>
2022-03-07 10:37:27 +0200

    Update container

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ export default function Home() {
   return (
     <div>
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>AutoSchool</title>
         <meta
           name="keywords"
