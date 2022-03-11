4cf7135798f6b9bd8aafd0d92cbec088d67e98b3
vofm <valerafm.vofm@gmail.com>
2022-03-11 07:34:31 +0200

    Style header position

--- components/HeaderLayout.js
+++ components/HeaderLayout.js
@@ -2,46 +2,34 @@ import s from "../styles/layout/header.module.scss";
 
 export function HeaderLayout() {
   return (
-    <header>
+    <header className={s.pageHeader}>
       <div className="container">
-        <div className={s.page_header}>
-          <img className={s.logo} src="images\logo\logo.png" width="45" s></img>
-          <span className={s.logo_title}>УМОУСЦ ТСО України</span>
-          <span className={s.logo_courses}>Курси водіїв</span>
-          <img className={s.location} src="location.svg" width="10"></img>
-          <span className={s.logo_city}>м. Ужгород</span>
-          <span className={s.logo_street}>вул. Підгірна, 28</span>
-          <img className={s.icon_menu} src="menu.svg"></img>
-          <img className={s.icon_menu} src="theme.svg"></img>
-        </div>
-        <img
-          className={s.bg_img}
-          src="images/background/header-background320.png"
-        ></img>
-        <img className={s.logo_car} src="images\logo\logo-car.png"></img>
-        <img className={s.icon_contacts} src="time.svg"></img>
-        <span className={s.shedule}>Пн-Пт 10:00 - 18:00</span>
-        <img className={s.icon_contacts} src="phone.svg"></img>
-        <p className={s.shedule}>
-          + 38 (099)-318-3873 + 38 (066)-088-6500 + 38 (099)-151-7035
-        </p>
-        <img className={s.icon_contacts} src="email.svg"></img>
-        <span className={s.shedule}> jaroslavafedulova@gmail.com</span>
-        <span className={s.title_course}>Ласкаво просимо на курси</span>
-        <h1 className={s.title_hero}>Автошкола</h1>
-        <p className={s.title_lessons}>
-          Пройди теоретичні та практичні заняття та отримай водійські права
-        </p>
-        <button className={s.btn}>Записатися онлайн на навчання</button>
-        <img className={s.icon_soc} src="viber.svg"></img>
-        <img className={s.icon_soc} src="facebook.svg"></img>
-        <img className={s.icon_soc} src="email.svg"></img>
-        <img className={s.icon_title} src="header-title.svg"></img>
-        <h3 className={s.title_guarantee}>Ми навчаємо з гарантією!</h3>
-        <p className={s.title_guaran_text}>
-          ТЕОРЕТИЧНІ ЗАНЯТТЯ стаціонарно та онлайн. ПРАКТИЧНІ ЗАНЯТТЯ -
-          ІНДИВІДУАЛЬНО З КОЖНИМ. Онлайн тести на сайті pdr.auto-tso.com
-        </p>
+        <nav className={s.pageHeader__nav}>
+          <div className={s.logo}>
+            <img
+              className={s.logo__img}
+              src="images\logo\logo.png"
+              width="45"
+            ></img>
+            <div className={s.title}>
+              <span className={s.title__organization}>УМОУСЦ ТСО України</span>
+              <span className={s.title__courses}>Курси водіїв</span>
+              <div className={s.location}>
+                <img
+                  className={s.location__icon}
+                  src="location.svg"
+                  width="10"
+                ></img>
+                <span className={s.location__city}>м. Ужгород</span>
+                <span className={s.location__street}>вул. Підгірна, 28</span>
+              </div>
+            </div>
+          </div>
+          <div className={s.menuNav}>
+            <img className={s.menuNav__icon} src="menu.svg"></img>
+            <img className={s.menuNav__icon} src="theme.svg"></img>
+          </div>
+        </nav>
       </div>
     </header>
   );
---
+++ components/HeroLayout.js
@@ -0,0 +1,38 @@
+import s from "../styles/layout/header.module.scss";
+
+export function HeaderLayout() {
+  return (
+    <section>
+      <div className="container">
+        <img
+          className={s.bg_img}
+          src="images/background/header-background320.png"
+        ></img>
+        <img className={s.logo_car} src="images\logo\logo-car.png"></img>
+        <img className={s.icon_contacts} src="time.svg"></img>
+        <span className={s.shedule}>Пн-Пт 10:00 - 18:00</span>
+        <img className={s.icon_contacts} src="phone.svg"></img>
+        <p className={s.shedule}>
+          + 38 (099)-318-3873 + 38 (066)-088-6500 + 38 (099)-151-7035
+        </p>
+        <img className={s.icon_contacts} src="email.svg"></img>
+        <span className={s.shedule}> jaroslavafedulova@gmail.com</span>
+        <span className={s.title_course}>Ласкаво просимо на курси</span>
+        <h1 className={s.title_hero}>Автошкола</h1>
+        <p className={s.title_lessons}>
+          Пройди теоретичні та практичні заняття та отримай водійські права
+        </p>
+        <button className={s.btn}>Записатися онлайн на навчання</button>
+        <img className={s.icon_soc} src="viber.svg"></img>
+        <img className={s.icon_soc} src="facebook.svg"></img>
+        <img className={s.icon_soc} src="email.svg"></img>
+        <img className={s.icon_title} src="header-title.svg"></img>
+        <h3 className={s.title_guarantee}>Ми навчаємо з гарантією!</h3>
+        <p className={s.title_guaran_text}>
+          ТЕОРЕТИЧНІ ЗАНЯТТЯ стаціонарно та онлайн. ПРАКТИЧНІ ЗАНЯТТЯ -
+          ІНДИВІДУАЛЬНО З КОЖНИМ. Онлайн тести на сайті pdr.auto-tso.com
+        </p>
+      </div>
+    </section>
+  );
+}
