4e765573f70fab63a4856a02a01552ea2557ca8a
vofm <valerafm.vofm@gmail.com>
2022-03-12 05:23:56 +0200

    gfdghfdgh

--- components/HeroLayout.js
+++ components/HeroLayout.js
@@ -4,10 +4,6 @@ export function HeroLayout() {
   return (
     <section>
       <div className="container">
-        <img
-          className={s.bg__img}
-          src="images/background/header-background320.png"
-        ></img>
         <div className={s.hero__contacts}>
           <img className={s.logo__car} src="images\logo\logo-car.png"></img>
           <div className={s.shedule}>
@@ -33,18 +29,28 @@ export function HeroLayout() {
             </div>
           </div>
         </div>
-        <p className={s.title__course}>Ласкаво просимо на курси</p>
-        <h1 className={s.title__hero}>Автошкола</h1>
-        <p className={s.title__lessons}>
-          Пройди теоретичні та практичні заняття та отримай водійські права
-        </p>
-        <button className={s.btn}>Записатися онлайн на навчання</button>
-        <img className={s.icon_soc} src="viber.svg"></img>
-        <img className={s.icon_soc} src="facebook.svg"></img>
-        <img className={s.icon_soc} src="email.svg"></img>
-        <img className={s.icon_title} src="header-title.svg"></img>
-        <h3 className={s.title_guarantee}>Ми навчаємо з гарантією!</h3>
-        <p className={s.title_guaran_text}>
+        <div className={s.section__autoschool}>
+          <img
+            className={s.bg__img}
+            src="images/background/header-background320.png"
+          ></img>
+          <p className={s.title__course}>Ласкаво просимо на курси</p>
+          <h1 className={s.title__hero}>Автошкола</h1>
+          <p className={s.title__lessons}>
+            Пройди теоретичні та практичні заняття та отримай водійські права
+          </p>
+          <button className={s.btn}>Записатися онлайн на навчання</button>
+          <div className={s.soc}>
+            <img className={s.soc__icon} src="viber.svg"></img>
+            <img className={s.soc__icon} src="facebook.svg"></img>
+            <img className={s.soc__icon} src="email.svg"></img>
+          </div>
+        </div>
+        <button className={s.btn__guarantee}>
+          <img className={s.guarantee__icon} src="header-title.svg"></img>
+        </button>
+        <h3 className={s.guarantee__title}>Ми навчаємо з гарантією!</h3>
+        <p className={s.guarantee_text}>
           ТЕОРЕТИЧНІ ЗАНЯТТЯ стаціонарно та онлайн. ПРАКТИЧНІ ЗАНЯТТЯ -
           ІНДИВІДУАЛЬНО З КОЖНИМ. Онлайн тести на сайті pdr.auto-tso.com
         </p>
