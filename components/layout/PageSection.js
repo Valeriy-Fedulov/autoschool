0bd9ef16a71c4e822a4fc13d729587033b904784
vofm <80212894+Valeriy-Fedulov@users.noreply.github.com>
2022-03-24 13:03:14 +0200

    Update style

--- components/layout/PageSection.js
+++ components/layout/PageSection.js
@@ -5,6 +5,17 @@ export default function PageSection() {
   return (
     <section>
       <div className="container">
+        <div className={s.pagesection__bg}>
+          <Image
+            alt="Mountains"
+            src="/images/background/BG-pagesection.jpg"
+            layout="fill"
+            objectFit="cover"
+            quality={100}
+            placeholder="bluer"
+            priority
+          />
+        </div>
         <div className="page__sections">
           <h2 className="title__sections">
             <span className="title__base">Що я можу отримати</span> із
@@ -20,44 +31,66 @@ export default function PageSection() {
                   руху.
                 </span>
               </p>
-              <ul>
-                <li>
+              <ul className={s.page_section__sublist}>
+                <li className={s.page_section__subtext}>
                   <div className={s.page_section__subbull}>1</div>
-                  <span className={s.page_section__subitem}>
-                    Теоретичний курс
-                  </span>{" "}
-                  по Правилам дорожнього руху та Основам безпеки дорожнього руху
-                  (закріпляючий знання ПДР).
+                  <p className={s.page_section__subp}>
+                    <span className={s.page_section__subitem}>
+                      Теоретичний курс
+                    </span>{" "}
+                    по Правилам дорожнього руху та Основам безпеки дорожнього
+                    руху (закріпляючий знання ПДР).
+                  </p>
                 </li>
-                <li>
-                  <span className={s.page_section__subitem}>Консультація</span>{" "}
-                  викладача.
+                <li className={s.page_section__subtext}>
+                  <div className={s.page_section__subbull}>2</div>
+                  <p className={s.page_section__subp}>
+                    <span className={s.page_section__subitem}>
+                      Консультація
+                    </span>{" "}
+                    викладача.
+                  </p>
                 </li>
-                <li>
-                  <span className={s.page_section__subitem}>Водіння</span> в
-                  умовах інтенсивного руху по оживленим напрямкам руху
-                  автомобілів міста, в тому числі і уроки безпечного водіння
-                  автомобіля.
+                <li className={s.page_section__subtext}>
+                  <div className={s.page_section__subbull}>3</div>
+                  <p className={s.page_section__subp}>
+                    <span className={s.page_section__subitem}>Водіння</span> в
+                    умовах інтенсивного руху по оживленим напрямкам руху
+                    автомобілів міста, в тому числі і уроки безпечного водіння
+                    автомобіля.
+                  </p>
                 </li>
-                <li>
-                  <span className={s.page_section__subitem}>
-                    Будова та експлуатація
-                  </span>{" "}
-                  транспортного засобу.
+                <li className={s.page_section__subtext}>
+                  <div className={s.page_section__subbull}>4</div>
+                  <p className={s.page_section__subp}>
+                    <span className={s.page_section__subitem}>
+                      Будова та експлуатація
+                    </span>{" "}
+                    транспортного засобу.
+                  </p>
                 </li>
-                <li>
-                  <span className={s.page_section__subitem}>
-                    Водіння з інструктором
-                  </span>{" "}
-                  (від дому до роботи, від дому до дитячого садка).
+                <li className={s.page_section__subtext}>
+                  <div className={s.page_section__subbull}>5</div>
+                  <p className={s.page_section__subp}>
+                    <span className={s.page_section__subitem}>
+                      Водіння з інструктором
+                    </span>{" "}
+                    (від дому до роботи, від дому до дитячого садка).
+                  </p>
                 </li>
-                <li>
-                  <span className={s.page_section__subitem}>Курс</span> “Права
-                  та обов’язки водія транспортного засобу”.
+                <li className={s.page_section__subtext}>
+                  <div className={s.page_section__subbull}>6</div>
+                  <p className={s.page_section__subp}>
+                    <span className={s.page_section__subitem}>Курс</span> “Права
+                    та обов’язки водія транспортного засобу”.
+                  </p>
                 </li>
-                <li>
-                  <span className={s.page_section__subitem}>Основи</span>{" "}
-                  безпеки руху.
+                <li className={s.page_section__subtext}>
+                  <div className={s.page_section__subbull}>7</div>
+                  <p className={s.page_section__subp}>
+                    <span className={s.page_section__subitem}>Основи</span>{" "}
+                    безпеки руху.
+                  </p>
                 </li>
               </ul>
             </li>
@@ -69,29 +102,43 @@ export default function PageSection() {
                   Але не мають практики водіння в зимовий час.
                 </span>
               </p>
-              <ul>
-                <li>
-                  <span className={s.page_section__subitem}>
-                    Теоретичний курс
-                  </span>{" "}
-                  по Правилам дорожнього руху та Основам безпеки дорожнього руху
-                  (встановлюючи знання ПДР).
+              <ul className={s.page_section__sublist}>
+                <li className={s.page_section__subtext}>
+                  <div className={s.page_section__subbull}>1</div>
+                  <p className={s.page_section__subp}>
+                    <span className={s.page_section__subitem}>
+                      Теоретичний курс
+                    </span>{" "}
+                    по Правилам дорожнього руху та Основам безпеки дорожнього
+                    руху (встановлюючи знання ПДР).
+                  </p>
                 </li>
-                <li>
-                  <span className={s.page_section__subitem}>Консультація</span>{" "}
-                  викладача.
+                <li className={s.page_section__subtext}>
+                  <div className={s.page_section__subbull}>2</div>
+                  <p className={s.page_section__subp}>
+                    <span className={s.page_section__subitem}>
+                      Консультація
+                    </span>{" "}
+                    викладача.
+                  </p>
                 </li>
-                <li>
-                  <span className={s.page_section__subitem}>Курс</span> “Права
-                  та обов’язки водія транспортного засобу”.
+                <li className={s.page_section__subtext}>
+                  <div className={s.page_section__subbull}>3</div>
+                  <p className={s.page_section__subp}>
+                    <span className={s.page_section__subitem}>Курс</span> “Права
+                    та обов’язки водія транспортного засобу”.
+                  </p>
                 </li>
-                <li>
-                  <span className={s.page_section__subitem}>
-                    Заняття по водінню
-                  </span>{" "}
-                  (водіння в умовах інтенсивного руху по оживленим напрямкам
-                  руху автомобілів міста), в тому числі і уроки безпечного
-                  водіння автомобіля.
+                <li className={s.page_section__subtext}>
+                  <div className={s.page_section__subbull}>4</div>
+                  <p className={s.page_section__subp}>
+                    <span className={s.page_section__subitem}>
+                      Заняття по водінню
+                    </span>{" "}
+                    (водіння в умовах інтенсивного руху по оживленим напрямкам
+                    руху автомобілів міста), в тому числі і уроки безпечного
+                    водіння автомобіля.
+                  </p>
                 </li>
               </ul>
             </li>
@@ -103,29 +150,43 @@ export default function PageSection() {
                   При керуванні автомобілем у міському потоці.
                 </span>
               </p>
-              <ul>
-                <li>
-                  <span className={s.page_section__subitem}>
-                    Теоретичний курс
-                  </span>{" "}
-                  по Правилам дорожнього руху та Основам безпеки дорожнього руху
-                  (встановлюючи знання ПДР).
+              <ul className={s.page_section__sublist}>
+                <li className={s.page_section__subtext}>
+                  <div className={s.page_section__subbull}>1</div>
+                  <p className={s.page_section__subp}>
+                    <span className={s.page_section__subitem}>
+                      Теоретичний курс
+                    </span>{" "}
+                    по Правилам дорожнього руху та Основам безпеки дорожнього
+                    руху (встановлюючи знання ПДР).
+                  </p>
                 </li>
-                <li>
-                  <span className={s.page_section__subitem}>Консультація</span>{" "}
-                  викладача.
+                <li className={s.page_section__subtext}>
+                  <div className={s.page_section__subbull}>2</div>
+                  <p className={s.page_section__subp}>
+                    <span className={s.page_section__subitem}>
+                      Консультація
+                    </span>{" "}
+                    викладача.
+                  </p>
                 </li>
-                <li>
-                  <span className={s.page_section__subitem}>Курс</span> “Права
-                  та обов’язки водія транспортного засобу”.
+                <li className={s.page_section__subtext}>
+                  <div className={s.page_section__subbull}>3</div>
+                  <p className={s.page_section__subp}>
+                    <span className={s.page_section__subitem}>Курс</span> “Права
+                    та обов’язки водія транспортного засобу”.
+                  </p>
                 </li>
-                <li>
-                  <span className={s.page_section__subitem}>
-                    Заняття по водінню
-                  </span>{" "}
-                  (водіння в умовах інтенсивного руху по оживленим напрямкам
-                  руху автомобілів міста), в тому числі і уроки безпечного
-                  водіння автомобіля.
+                <li className={s.page_section__subtext}>
+                  <div className={s.page_section__subbull}>4</div>
+                  <p className={s.page_section__subp}>
+                    <span className={s.page_section__subitem}>
+                      Заняття по водінню
+                    </span>{" "}
+                    (водіння в умовах інтенсивного руху по оживленим напрямкам
+                    руху автомобілів міста), в тому числі і уроки безпечного
+                    водіння автомобіля.
+                  </p>
                 </li>
               </ul>
             </li>
