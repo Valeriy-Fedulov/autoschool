ef02c89a858b2a77cb0128d33dc3e36aab4a7716
vofm <valerafm.vofm@gmail.com>
2022-03-25 07:40:23 +0200

    Add style gallery

--- components/layout/Gallery.js
+++ components/layout/Gallery.js
@@ -9,6 +9,16 @@ export default function Gallery() {
           <h2 className="title__sections">
             <span className="title__base">Фотографії</span> автошколи
           </h2>
+          <div className={s.gallery__item}>
+            <Image
+              src="/gallery/img-1.jpeg"
+              layout="fill"
+              objectFit="cover"
+              quality={100}
+              alt="Фотографія автошколи"
+              placeholder="bluer"
+            ></Image>
+          </div>
         </div>
       </div>
     </section>
