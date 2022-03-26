8b20a2cd2a770f9f6ff0929854d1cecbfc8c4450
vofm <valerafm.vofm@gmail.com>
2022-03-26 08:27:40 +0200

    Update style

--- components/layout/Gallery.js
+++ components/layout/Gallery.js
@@ -5,19 +5,57 @@ export default function Gallery() {
   return (
     <section>
       <div className="container">
+        <div className={s.category__bg}>
+          <Image
+            alt="Mountains"
+            src="/images/background/BG-gallery.jpeg"
+            layout="fill"
+            objectFit="cover"
+            quality={100}
+            placeholder="bluer"
+            priority
+          />
+        </div>
         <div className="page__sections">
           <h2 className="title__sections">
             <span className="title__base">Фотографії</span> автошколи
           </h2>
-          <div className={s.gallery__item}>
-            <Image
-              src="/gallery/img-1.jpeg"
-              layout="fill"
-              objectFit="cover"
-              quality={100}
-              alt="Фотографія автошколи"
-              placeholder="bluer"
-            ></Image>
+          <div className={s.gallery__border}>
+            <div className={s.gallery__item}>
+              <Image
+                src="/gallery/img-1.jpeg"
+                layout="fill"
+                objectFit="cover"
+                quality={100}
+                alt="Фотографія автошколи"
+                placeholder="bluer"
+              />
+            </div>
+          </div>
+          <div className={s.gallery__nav}>
+            <button className={s.btn__img}>
+              <div className={s.btn__arrow}>
+                <Image
+                  src="/arrow-left.svg"
+                  width={17}
+                  height={17}
+                  alt="Стрілка вліво"
+                  placeholder="bluer"
+                ></Image>
+              </div>
+            </button>
+            <p className={s.gallery__bull}>&bull; &bull; &bull;</p>
+            <button className={s.btn__img}>
+              <div className={s.btn__arrow}>
+                <Image
+                  src="/arrow-right.svg"
+                  width={17}
+                  height={17}
+                  alt="Стрілка вліво"
+                  placeholder="bluer"
+                ></Image>
+              </div>
+            </button>
           </div>
         </div>
       </div>
