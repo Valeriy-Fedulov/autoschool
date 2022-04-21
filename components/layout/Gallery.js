7883f27f927c3afa17f75f1bbd7c350f88bcb9ef
vofm <80212894+Valeriy-Fedulov@users.noreply.github.com>
2022-04-21 11:36:23 +0300

    Fix Gallery and Teachers

--- components/layout/Gallery.js
+++ components/layout/Gallery.js
@@ -20,18 +20,35 @@ export default function Gallery() {
           <h2 className="title__sections">
             <span className="title__base">Фотографії</span> автошколи
           </h2>
-          <div className={s.gallery__border}>
-            <div className={s.gallery__item}>
-              <Image
-                src="/gallery/img-1.jpeg"
-                layout="fill"
-                objectFit="cover"
-                quality={100}
-                alt="Фотографія автошколи"
-                placeholder="bluer"
-              />
+
+          <div className={s.gallery__wrapper}>
+            <div className={s.gallery__border}>
+              <div className={s.gallery__item}>
+                <Image
+                  src="/gallery/img-1.jpeg"
+                  layout="fill"
+                  objectFit="cover"
+                  quality={100}
+                  alt="Фотографія автошколи"
+                  placeholder="bluer"
+                />
+              </div>
+            </div>
+
+            <div className={s.gallery__border}>
+              <div className={s.gallery__item}>
+                <Image
+                  src="/gallery/img-1.jpeg"
+                  layout="fill"
+                  objectFit="cover"
+                  quality={100}
+                  alt="Фотографія автошколи"
+                  placeholder="bluer"
+                />
+              </div>
             </div>
           </div>
+
           <div className={s.gallery__nav}>
             <button className={s.btn__img}>
               <div className={s.btn__arrow}>
