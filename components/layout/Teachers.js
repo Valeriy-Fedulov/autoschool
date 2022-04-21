1288a24ab601a1afde72038002be9cc593f82d38
vofm <valerafm.vofm@gmail.com>
2022-04-21 07:00:44 +0300

    Add styles Teachers big-mob

--- components/layout/Teachers.js
+++ components/layout/Teachers.js
@@ -20,20 +20,42 @@ export default function Teachers() {
           <h2 className="title__sections">
             Наші педагогічні <span className="title__base">працівники</span>
           </h2>
-          <div className={s.teachers__shadow}>
-            <div className={s.teachers__foto}>
-              <Image
-                src="/foto/teacher.jpeg"
-                width={225}
-                height={225}
-                alt="Викладач"
-                placeholder="bluer"
-              />
+
+          <div className={s.teachers__wrapper}>
+            <div>
+              <div className={s.teachers__shadow}>
+                <div className={s.teachers__foto}>
+                  <Image
+                    src="/foto/teacher.jpeg"
+                    width={225}
+                    height={225}
+                    alt="Викладач"
+                    placeholder="bluer"
+                  />
+                </div>
+              </div>
+              <h3 className={s.teachers__name}>Дмитрій Іванов</h3>
+              <p className={s.teachers__position}>Теоретичні занятя</p>
+              <button className={s.teachers__btn}>Інструктор</button>
+            </div>
+
+            <div>
+              <div className={s.teachers__shadow}>
+                <div className={s.teachers__foto}>
+                  <Image
+                    src="/foto/teacher.jpeg"
+                    width={225}
+                    height={225}
+                    alt="Викладач"
+                    placeholder="bluer"
+                  />
+                </div>
+              </div>
+              <h3 className={s.teachers__name}>Дмитрій Іванов</h3>
+              <p className={s.teachers__position}>Теоретичні занятя</p>
+              <button className={s.teachers__btn}>Інструктор</button>
             </div>
           </div>
-          <h3 className={s.teachers__name}>Дмитрій Іванов</h3>
-          <p className={s.teachers__position}>Теоретичні занятя</p>
-          <button className={s.teachers__btn}>Інструктор</button>
         </div>
       </div>
     </section>
