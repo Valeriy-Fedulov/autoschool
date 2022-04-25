7b00a54e95786d75091c6e6f3232552be70b91bb
vofm <valerafm.vofm@gmail.com>
2022-04-25 06:05:11 +0300

    Update

--- components/Button.jsx
+++ components/Button.jsx
@@ -2,6 +2,5 @@ import Image from "next/image";
 import s from "../styles/components/Button.module.scss";
 
 export default function Button() {
-  console.log(value);
-  return <button>guyhjk</button>;
+  return <button>{children}</button>;
 }
