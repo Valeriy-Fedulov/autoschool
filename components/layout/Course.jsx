c6bc50cc4b43b40c644e38cd0a36c74eed91a80b
vofm <80212894+Valeriy-Fedulov@users.noreply.github.com>
2022-04-26 16:57:14 +0300

    Update

--- components/layout/Course.jsx
+++ components/layout/Course.jsx
@@ -21,212 +21,216 @@ export default function Course() {
             Чому наш курс? <span className="title__base">Все просто!</span>
           </h2>
           <ul className={s.course__list}>
-            <li className={s.course__item}>
-              <div className={s.course__icon}>
-                <Image
-                  src="/circle-item.svg"
-                  width={35}
-                  height={35}
-                  alt="Пункт"
-                  placeholder="bluer"
-                />
-              </div>
-              Тривалість навчання в залежності від категорії від 2 тижнів до 3
-              місяців.
-            </li>
-            <li className={s.course__item}>
-              <div className={s.course__icon}>
-                <Image
-                  src="/circle-item.svg"
-                  width={35}
-                  height={35}
-                  alt="Пункт"
-                  placeholder="bluer"
-                />
-              </div>
-              Наявність інтенсивної програми.
-            </li>
-            <li className={s.course__item}>
-              <div className={s.course__icon}>
-                <Image
-                  src="/circle-item.svg"
-                  width={35}
-                  height={35}
-                  alt="Пункт"
-                  placeholder="bluer"
-                />
-              </div>
-              Можливість безкоштовно повторно відвідати любу лекцію.
-            </li>
-            <li className={s.course__item}>
-              <div className={s.course__icon}>
-                <Image
-                  src="/circle-item.svg"
-                  width={35}
-                  height={35}
-                  alt="Пункт"
-                  placeholder="bluer"
-                />
-              </div>
-              Вибір місця відвідування занять.
-            </li>
-            <li className={s.course__item}>
-              <div className={s.course__icon}>
-                <Image
-                  src="/circle-item.svg"
-                  width={35}
-                  height={35}
-                  alt="Пункт"
-                  placeholder="bluer"
-                />
-              </div>
-              Вибір графіка відвідування занять.
-            </li>
-            <li className={s.course__item}>
-              <div className={s.course__icon}>
-                <Image
-                  src="/circle-item.svg"
-                  width={35}
-                  height={35}
-                  alt="Пункт"
-                  placeholder="bluer"
-                />
-              </div>
-              Вибір форми навчання.
-            </li>
-            <li className={s.course__item}>
-              <div className={s.course__icon}>
-                <Image
-                  src="/circle-item.svg"
-                  width={35}
-                  height={35}
-                  alt="Пункт"
-                  placeholder="bluer"
-                />
-              </div>
-              При неможливості відвідування якої небуть лекції по ПДР -
-              безкоштовне відвідування пропущеної лекції, допомога в складанні
-              індивідуального графіка лекцій.
-            </li>
-            <li className={s.course__item}>
-              <div className={s.course__icon}>
-                <Image
-                  src="/circle-item.svg"
-                  width={35}
-                  height={35}
-                  alt="Пункт"
-                  placeholder="bluer"
-                />
-              </div>
-              Особливості навчання - примінення сучасних технічних засобів, в
-              т.ч. засобів інтерактивного моделювання ситуації.
-            </li>
-            <li className={s.course__item}>
-              <div className={s.course__icon}>
-                <Image
-                  src="/circle-item.svg"
-                  width={35}
-                  height={35}
-                  alt="Пункт"
-                  placeholder="bluer"
-                />
-              </div>
-              Навчання із врахуванням жіночої психології.
-            </li>
-            <li className={s.course__item}>
-              <div className={s.course__icon}>
-                <Image
-                  src="/circle-item.svg"
-                  width={35}
-                  height={35}
-                  alt="Пункт"
-                  placeholder="bluer"
-                />
-              </div>
-              Кількість занять по водінню визначається з врахуванням наявності
-              навиків та індивідуальних здібностей.
-            </li>
-            <li className={s.course__item}>
-              <div className={s.course__icon}>
-                <Image
-                  src="/circle-item.svg"
-                  width={35}
-                  height={35}
-                  alt="Пункт"
-                  placeholder="bluer"
-                />
-              </div>
-              Водіння в різних районах Ужгорода та Ужгородського району -
-              можливість комбінувати, наприклад: водіння поруч з проживанням,
-              роботою.
-            </li>
-            <li className={s.course__item}>
-              <div className={s.course__icon}>
-                <Image
-                  src="/circle-item.svg"
-                  width={35}
-                  height={35}
-                  alt="Пункт"
-                  placeholder="bluer"
-                />
-              </div>
-              Гарантія на навчання<span className={s.footnote}>*</span>
-              <p className={s.footnote__text}>
-                <span className={s.footnote}>*</span> Безкоштовне повторне
-                навчання на протязі року по бажанню слухача в залежності від
-                результату здачі екзамену.
-              </p>
-            </li>
-            <li className={s.course__item}>
-              <div className={s.course__icon}>
-                <Image
-                  src="/circle-item.svg"
-                  width={35}
-                  height={35}
-                  alt="Пункт"
-                  placeholder="bluer"
-                />
-              </div>
-              Обов’язкове проходження викладачами та інструкторами з практичного
-              керування автомобіля підвищення кваліфікації
-            </li>
-            <li className={s.course__item}>
-              <div className={s.course__icon}>
-                <Image
-                  src="/circle-item.svg"
-                  width={35}
-                  height={35}
-                  alt="Пункт"
-                  placeholder="bluer"
-                />
-              </div>
-              Здача екзаменів в ДАІ - в зручний Вам день та час зразу після
-              закінчення курсів.
-            </li>
-            <li className={s.course__item}>
-              <div className={s.course__icon}>
-                <Image
-                  src="/circle-item.svg"
-                  width={35}
-                  height={35}
-                  alt="Пункт"
-                  placeholder="bluer"
-                />
-              </div>
-              Безкоштовні консультації з викладачем по ПДР.
-            </li>
-            <li className={s.course__item}>
-              <div className={s.course__icon}>
-                <Image
-                  src="/circle-item.svg"
-                  width={35}
-                  height={35}
-                  alt="Пункт"
-                  placeholder="bluer"
-                />
-              </div>
-              Практичні заняття з інструктором по контрольному маршруту ДАІ.
-            </li>
+            <div>
+              <li className={s.course__item}>
+                <div className={s.course__icon}>
+                  <Image
+                    src="/circle-item.svg"
+                    width={35}
+                    height={35}
+                    alt="Пункт"
+                    placeholder="bluer"
+                  />
+                </div>
+                Тривалість навчання в залежності від категорії від 2 тижнів до 3
+                місяців.
+              </li>
+              <li className={s.course__item}>
+                <div className={s.course__icon}>
+                  <Image
+                    src="/circle-item.svg"
+                    width={35}
+                    height={35}
+                    alt="Пункт"
+                    placeholder="bluer"
+                  />
+                </div>
+                Наявність інтенсивної програми.
+              </li>
+              <li className={s.course__item}>
+                <div className={s.course__icon}>
+                  <Image
+                    src="/circle-item.svg"
+                    width={35}
+                    height={35}
+                    alt="Пункт"
+                    placeholder="bluer"
+                  />
+                </div>
+                Можливість безкоштовно повторно відвідати любу лекцію.
+              </li>
+              <li className={s.course__item}>
+                <div className={s.course__icon}>
+                  <Image
+                    src="/circle-item.svg"
+                    width={35}
+                    height={35}
+                    alt="Пункт"
+                    placeholder="bluer"
+                  />
+                </div>
+                Вибір місця відвідування занять.
+              </li>
+              <li className={s.course__item}>
+                <div className={s.course__icon}>
+                  <Image
+                    src="/circle-item.svg"
+                    width={35}
+                    height={35}
+                    alt="Пункт"
+                    placeholder="bluer"
+                  />
+                </div>
+                Вибір графіка відвідування занять.
+              </li>
+              <li className={s.course__item}>
+                <div className={s.course__icon}>
+                  <Image
+                    src="/circle-item.svg"
+                    width={35}
+                    height={35}
+                    alt="Пункт"
+                    placeholder="bluer"
+                  />
+                </div>
+                Вибір форми навчання.
+              </li>
+              <li className={s.course__item}>
+                <div className={s.course__icon}>
+                  <Image
+                    src="/circle-item.svg"
+                    width={35}
+                    height={35}
+                    alt="Пункт"
+                    placeholder="bluer"
+                  />
+                </div>
+                При неможливості відвідування якої небуть лекції по ПДР -
+                безкоштовне відвідування пропущеної лекції, допомога в складанні
+                індивідуального графіка лекцій.
+              </li>
+              <li className={s.course__item}>
+                <div className={s.course__icon}>
+                  <Image
+                    src="/circle-item.svg"
+                    width={35}
+                    height={35}
+                    alt="Пункт"
+                    placeholder="bluer"
+                  />
+                </div>
+                Особливості навчання - примінення сучасних технічних засобів, в
+                т.ч. засобів інтерактивного моделювання ситуації.
+              </li>
+              <li className={s.course__item}>
+                <div className={s.course__icon}>
+                  <Image
+                    src="/circle-item.svg"
+                    width={35}
+                    height={35}
+                    alt="Пункт"
+                    placeholder="bluer"
+                  />
+                </div>
+                Навчання із врахуванням жіночої психології.
+              </li>
+            </div>
+            <div>
+              <li className={s.course__item}>
+                <div className={s.course__icon}>
+                  <Image
+                    src="/circle-item.svg"
+                    width={35}
+                    height={35}
+                    alt="Пункт"
+                    placeholder="bluer"
+                  />
+                </div>
+                Кількість занять по водінню визначається з врахуванням наявності
+                навиків та індивідуальних здібностей.
+              </li>
+              <li className={s.course__item}>
+                <div className={s.course__icon}>
+                  <Image
+                    src="/circle-item.svg"
+                    width={35}
+                    height={35}
+                    alt="Пункт"
+                    placeholder="bluer"
+                  />
+                </div>
+                Водіння в різних районах Ужгорода та Ужгородського району -
+                можливість комбінувати, наприклад: водіння поруч з проживанням,
+                роботою.
+              </li>
+              <li className={s.course__item}>
+                <div className={s.course__icon}>
+                  <Image
+                    src="/circle-item.svg"
+                    width={35}
+                    height={35}
+                    alt="Пункт"
+                    placeholder="bluer"
+                  />
+                </div>
+                Гарантія на навчання<span className={s.footnote}>*</span>
+                <p className={s.footnote__text}>
+                  <span className={s.footnote}>*</span> Безкоштовне повторне
+                  навчання на протязі року по бажанню слухача в залежності від
+                  результату здачі екзамену.
+                </p>
+              </li>
+              <li className={s.course__item}>
+                <div className={s.course__icon}>
+                  <Image
+                    src="/circle-item.svg"
+                    width={35}
+                    height={35}
+                    alt="Пункт"
+                    placeholder="bluer"
+                  />
+                </div>
+                Обов’язкове проходження викладачами та інструкторами з
+                практичного керування автомобіля підвищення кваліфікації
+              </li>
+              <li className={s.course__item}>
+                <div className={s.course__icon}>
+                  <Image
+                    src="/circle-item.svg"
+                    width={35}
+                    height={35}
+                    alt="Пункт"
+                    placeholder="bluer"
+                  />
+                </div>
+                Здача екзаменів в ДАІ - в зручний Вам день та час зразу після
+                закінчення курсів.
+              </li>
+              <li className={s.course__item}>
+                <div className={s.course__icon}>
+                  <Image
+                    src="/circle-item.svg"
+                    width={35}
+                    height={35}
+                    alt="Пункт"
+                    placeholder="bluer"
+                  />
+                </div>
+                Безкоштовні консультації з викладачем по ПДР.
+              </li>
+              <li className={s.course__item}>
+                <div className={s.course__icon}>
+                  <Image
+                    src="/circle-item.svg"
+                    width={35}
+                    height={35}
+                    alt="Пункт"
+                    placeholder="bluer"
+                  />
+                </div>
+                Практичні заняття з інструктором по контрольному маршруту ДАІ.
+              </li>
+            </div>
           </ul>
           <button className="btn">Записатися онлайн на навчання</button>
         </div>
